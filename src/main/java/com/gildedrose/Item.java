f605146d849022057ea2b794829f5c4f29b82c48
Stephane <stephane.dony@telenetgroup.be>
2019-03-17 14:54:24 +0100

    Refactoring Item.java , private variables, getter and setters

--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -1,21 +1,67 @@
 package com.gildedrose;
 
+import java.util.Objects;
+
 public class Item {
 
-    public String name;
+    private String name;
+    private int sellIn;
+    private int quality;
 
-    public int sellIn;
+    private Item() {
 
-    public int quality;
+    }
 
-    public Item(String name, int sellIn, int quality) {
+    private Item(String name, int sellIn, int quality) {
         this.name = name;
         this.sellIn = sellIn;
         this.quality = quality;
     }
 
-   @Override
-   public String toString() {
+    public String getName() {
+        return name;
+    }
+
+    public int getSellIn() {
+        return sellIn;
+    }
+
+    public int getQuality() {
+        return quality;
+    }
+
+    public Item setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public Item setSellIn(int sellIn) {
+        this.sellIn = sellIn;
+        return this;
+    }
+
+    public Item setQuality(int quality) {
+        this.quality = quality;
+        return this;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return sellIn == item.sellIn &&
+                quality == item.quality &&
+                Objects.equals(name, item.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, sellIn, quality);
+    }
+
+    @Override
+    public String toString() {
         return this.name + ", " + this.sellIn + ", " + this.quality;
     }
 }
